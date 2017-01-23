b8a47f7a67583307989963de8a0af6e929bcd322
axl002.ta <axl002.ta@gmail.com>
2017-01-23 14:24:06 -0800

    first commit

---
+++ out/production/apitest/compile_no_run.sh
@@ -0,0 +1,1 @@
+javac -cp dankjson.jar Dankapi.java
---
+++ start_somewhere.sh
@@ -0,0 +1,2 @@
+javac -cp dankjson.jar Dankapi.java
+java -cp .:dankjson.jar Dankapi '29610174-30523316-29197805-30064084-30034654' 'hugedump/' '72'
