27e47f53dd8d42e7dcb959f0f0504530e3c4b25f
Ubuntu <ubuntu@ip-172-31-2-229.us-west-2.compute.internal>
2017-01-23 23:53:39 +0000

    error 503 resume

--- start_somewhere.sh
+++ start_somewhere.sh
@@ -1,2 +1,2 @@
 javac -cp dankjson.jar Dankapi.java
-java -cp .:dankjson.jar Dankapi '7027898-7496390-6922667-8200030-7630573' 'hugedump/' '10000'
+java -cp .:dankjson.jar Dankapi '7230025-7789167-7113634-8480515-7907691' 'hugedump/' '10000'
