1a47761e7a3603ef39c990b39180bb75561998c0
axl002.ta <axl002.ta@gmail.com>
2017-01-24 15:40:21 -0800

    help

--- DankProducer.java
+++ DankProducer.java
@@ -20,44 +20,44 @@ import kafka.producer.ProducerConfig;
 
 public class DankProducer {
 
-//    public static void main(String[] args){
-//        Properties props = new Properties();
-//        props.put("metadata.broker.list", "broker1:9092,broker2:9092");
-//        props.put("acks", "all");
-//        props.put("retries", 0);
-//        props.put("batch.size", 16384);
-//        props.put("linger.ms", 1);
-//        props.put("buffer.memory", 33554432);
-//        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-//        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-//
-//        Producer<String, String> producer = new KafkaProducer<>(props);
-//        for(int i = 0; i < 100; i++)
-//            producer.send(new ProducerRecord<String, String>("my-topic", Integer.toString(i), Integer.toString(i)));
-//
-//        producer.close();
-
-    public static void main(String[] args) {
-        long events = Long.parseLong(args[0]);
-        Random rnd = new Random();
-
+    public static void main(String[] args){
         Properties props = new Properties();
-        props.put("metadata.broker.list", "broker1:9092,broker2:9092 ");
-        props.put("serializer.class", "kafka.serializer.StringEncoder");
-        props.put("partitioner.class", "SimplePartitioner");
-        props.put("request.required.acks", "1");
+        props.put("metadata.broker.list", "broker1:9092,broker2:9092");
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
-        ProducerConfig config = new ProducerConfig(props);
+        Producer<String, String> producer = new KafkaProducer<>(props);
+        for(int i = 0; i < 100; i++)
+            producer.send(new ProducerRecord<String, String>("my-topic", Integer.toString(i), Integer.toString(i)));
 
-        Producer<String, String> producer = new KafkaProducer<String, String>(config);
-
-        for (long nEvents = 0; nEvents < events; nEvents++) {
-            long runtime = new Date().getTime();
-            String ip = "192.168.2." + rnd.nextInt(255);
-            String msg = runtime + ",www.example.com," + ip;
-            KeyedMessage<String, String> data = new KeyedMessage<String, String>("page_visits", ip, msg);
-            producer.send(data);
-        }
         producer.close();
-    }
+
+//    public static void main(String[] args) {
+//        long events = Long.parseLong(args[0]);
+//        Random rnd = new Random();
+//
+//        Properties props = new Properties();
+//        props.put("metadata.broker.list", "broker1:9092,broker2:9092 ");
+//        props.put("serializer.class", "kafka.serializer.StringEncoder");
+//        props.put("partitioner.class", "SimplePartitioner");
+//        props.put("request.required.acks", "1");
+//
+//        ProducerConfig config = new ProducerConfig(props);
+//
+//        Producer<String, String> producer = new KafkaProducer<String, String>(config);
+//
+//        for (long nEvents = 0; nEvents < events; nEvents++) {
+//            long runtime = new Date().getTime();
+//            String ip = "192.168.2." + rnd.nextInt(255);
+//            String msg = runtime + ",www.example.com," + ip;
+//            KeyedMessage<String, String> data = new KeyedMessage<String, String>("page_visits", ip, msg);
+//            producer.send(data);
+//        }
+//        producer.close();
+//    }
 }
