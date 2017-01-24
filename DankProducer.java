aee6683bb727df8aaef57600ee0f789cae8c5a0b
axl002.ta <axl002.ta@gmail.com>
2017-01-23 21:42:59 -0800

    test import call for kafka

--- DankProducer.java
+++ DankProducer.java
@@ -35,7 +35,7 @@ public class DankProducer {
         Properties props = new Properties();
         props.put("metadata.broker.list", "broker1:9092,broker2:9092 ");
         props.put("serializer.class", "kafka.serializer.StringEncoder");
-        props.put("partitioner.class", "SimplePartitioner");
+        //props.put("partitioner.class", "SimplePartitioner");
         props.put("request.required.acks", "1");
 
         ProducerConfig config = new ProducerConfig(props);
@@ -50,5 +50,5 @@ public class DankProducer {
             producer.send(data);
         }
         producer.close();
-    }   
+    }
 }
