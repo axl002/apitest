2683d4791dda350bee0865841ebee10f1b3dd369
axl002.ta <axl002.ta@gmail.com>
2017-01-23 19:05:09 -0800

    error 503 updated start somewhere for resume

--- start_somewhere.sh
+++ start_somewhere.sh
@@ -1,2 +1,2 @@
 javac -cp dankjson.jar Dankapi.java
-java -cp .:dankjson.jar Dankapi '7230025-7789167-7113634-8480515-7907691' '/user/ubuntu/hugedump/' '10000'
+java -cp .:dankjson.jar Dankapi '37551862-39520680-37188961-41593406-39646941' '/user/ubuntu/hugedump/' '7000'
