dba35e01ab3a5d9c64bbc2b3a690124e0e723811
axl002.ta <axl002.ta@gmail.com>
2017-01-23 21:40:11 -0800

    test import call for kafka

--- DankProducer.java
+++ DankProducer.java
@@ -50,24 +50,5 @@ public class DankProducer {
             producer.send(data);
         }
         producer.close();
-    }
-
-
-
-    public class SimplePartitioner implements Partitioner {
-        public SimplePartitioner (VerifiableProperties props) {
-
-        }
-
-        public int partition(Object key, int a_numPartitions) {
-            int partition = 0;
-            String stringKey = (String) key;
-            int offset = stringKey.lastIndexOf('.');
-            if (offset > 0) {
-                partition = Integer.parseInt( stringKey.substring(offset+1)) % a_numPartitions;
-            }
-            return partition;
-        }
-
-    }
+    }   
 }
---
+++ SimplePartitioner.java
@@ -0,0 +1,22 @@
+//import util.properties packages
+import java.util.*;
+
+//import simple producer packages
+import kafka.*;
+
+public class SimplePartitioner implements Partitioner {
+    public SimplePartitioner (VerifiableProperties props) {
+
+    }
+
+    public int partition(Object key, int a_numPartitions) {
+        int partition = 0;
+        String stringKey = (String) key;
+        int offset = stringKey.lastIndexOf('.');
+        if (offset > 0) {
+            partition = Integer.parseInt( stringKey.substring(offset+1)) % a_numPartitions;
+        }
+        return partition;
+    }
+
+}
