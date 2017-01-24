cb747e03d884d4cd99b79d070a741845e746b175
axl002.ta <axl002.ta@gmail.com>
2017-01-24 12:37:17 -0800

    help

--- DankProducer.java
+++ DankProducer.java
@@ -8,6 +8,8 @@ import java.lang.*;
 
 //import simple producer packages
 import kafka.javaapi.producer.Producer;
+import kafka.javaapi.producer.KeyedMessage;
+import kafka.javaapi.producer.ProducerConfig;
 import kafka.javaapi.*;
 import kafka.producer.KeyedMessage;
 import kafka.producer.ProducerConfig;
