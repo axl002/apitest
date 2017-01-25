c51a858886121f04f60b8dac1e2fee552c181911
axl002.ta <axl002.ta@gmail.com>
2017-01-24 16:12:57 -0800

    commit DankConsumer

---
+++ DankConsumer.java
@@ -0,0 +1,62 @@
+/**
+ * Created by user on 1/24/17.
+ */
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import kafka.consumer.ConsumerConfig;
+import kafka.consumer.ConsumerIterator;
+import kafka.consumer.KafkaStream;
+import kafka.javaapi.consumer.ConsumerConnector;
+
+public class DankConsumer {
+    private ConsumerConnector consumerConnector = null;
+    private final String topic = "my-topic";
+
+    public void initialize() {
+        Properties props = new Properties();
+        props.put("zookeeper.connect", "ec2-52-32-13-253.us-west-2.compute.amazonaws.com:2181");
+        props.put("group.id", "None");
+        props.put("zookeeper.session.timeout.ms", "400");
+        props.put("zookeeper.sync.time.ms", "300");
+        props.put("auto.commit.interval.ms", "100");
+        ConsumerConfig conConfig = new ConsumerConfig(props);
+        consumerConnector = kafka.consumer.Consumer.createJavaConsumerConnector(conConfig);
+    }
+
+    public void consume() {
+        //Key = topic name, Value = No. of threads for topic
+        Map<String, Integer> topicCount = new HashMap<String, Integer>();
+        topicCount.put(topic, new Integer(1));
+
+        //ConsumerConnector creates the message stream for each topic
+        Map<String, List<KafkaStream<byte[], byte[]>>> consumerStreams =
+                consumerConnector.createMessageStreams(topicCount);
+
+        // Get Kafka stream for topic 'mytopic'
+        List<KafkaStream<byte[], byte[]>> kStreamList =
+                consumerStreams.get(topic);
+        // Iterate stream using ConsumerIterator
+        for (final KafkaStream<byte[], byte[]> kStreams : kStreamList) {
+            ConsumerIterator<byte[], byte[]> consumerIte = kStreams.iterator();
+
+            while (consumerIte.hasNext())
+                System.out.println("Message consumed from topic[" + topic + "] : "       +
+                        new String(consumerIte.next().message()));
+        }
+        //Shutdown the consumer connector
+        if (consumerConnector != null)   consumerConnector.shutdown();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Consumer consumer = new Consumer();
+        // Configure Kafka consumer
+        consumer.initialize();
+        // Start consumption
+        consumer.consume();
+    }
+
+}
