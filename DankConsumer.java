8346c6c764e6411f3decf743bf3af5f8d5784ea1
axl002.ta <axl002.ta@gmail.com>
2017-01-24 16:39:37 -0800

    commit DankConsumer

--- DankConsumer.java
+++ DankConsumer.java
@@ -2,64 +2,40 @@
  * Created by user on 1/24/17.
  */
 
-package kafka;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Properties;
-
-import kafka.consumer.ConsumerConfig;
-import kafka.consumer.ConsumerIterator;
-import kafka.consumer.KafkaStream;
-import kafka.javaapi.consumer.ConsumerConnector;
+import java.util.Arrays;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 public class DankConsumer {
-    private ConsumerConnector consumerConnector = null;
-    private final String topic = "my-topic";
+    public static void main(String[] args) throws Exception {
 
-    public void initialize() {
+
+        String topic = "my-topic";
+        String group = args[1].toString();
         Properties props = new Properties();
-        props.put("zookeeper.connect", "ec2-52-32-13-253.us-west-2.compute.amazonaws.com:2181");
-        props.put("group.id", "None");
-        props.put("zookeeper.session.timeout.ms", "400");
-        props.put("zookeeper.sync.time.ms", "300");
-        props.put("auto.commit.interval.ms", "100");
-        ConsumerConfig conConfig = new ConsumerConfig(props);
-        consumerConnector = kafka.consumer.Consumer.createJavaConsumerConnector(conConfig);
-    }
+        props.put("bootstrap.servers", "localhost:9092");
+        //props.put("group.id", group);
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("session.timeout.ms", "30000");
+        props.put("key.deserializer",
+                "org.apache.kafka.common.serializa-tion.StringDeserializer");
+        props.put("value.deserializer",
+                "org.apache.kafka.common.serializa-tion.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
 
-    public void consume() {
-        //Key = topic name, Value = No. of threads for topic
-        Map<String, Integer> topicCount = new HashMap<String, Integer>();
-        topicCount.put(topic, new Integer(1));
+        consumer.subscribe(Arrays.asList(topic));
+        System.out.println("Subscribed to topic " + topic);
+        int i = 0;
 
-        //ConsumerConnector creates the message stream for each topic
-        Map<String, List<KafkaStream<byte[], byte[]>>> consumerStreams =
-                consumerConnector.createMessageStreams(topicCount);
-
-        // Get Kafka stream for topic 'mytopic'
-        List<KafkaStream<byte[], byte[]>> kStreamList =
-                consumerStreams.get(topic);
-        // Iterate stream using ConsumerIterator
-        for (final KafkaStream<byte[], byte[]> kStreams : kStreamList) {
-            ConsumerIterator<byte[], byte[]> consumerIte = kStreams.iterator();
-
-            while (consumerIte.hasNext())
-                System.out.println("Message consumed from topic[" + topic + "] : "       +
-                        new String(consumerIte.next().message()));
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            for (ConsumerRecord<String, String> record : records)
+                System.out.printf("offset = %d, key = %s, value = %s\n",
+                        record.offset(), record.key(), record.value());
         }
-        //Shutdown the consumer connector
-        if (consumerConnector != null)   consumerConnector.shutdown();
-    }
-
-    public static void main(String[] args) throws InterruptedException {
-        Consumer consumer = new KafkaConsumer();
-        // Configure Kafka consumer
-        consumer.initialize();
-        // Start consumption
-        consumer.consume();
     }
 
 }
